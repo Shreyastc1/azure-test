2cdb20327f3d8c9c36c209d77b820f908e7f9490
Shreyastc1 <135942784+Shreyastc1@users.noreply.github.com>
2024-05-09 15:12:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,15 +2,12 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
-  location = var.location
-}
+
 
 resource "azurerm_storage_account" "example" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = "Terraform-RG"
+  location                 = "Central India"
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
 
