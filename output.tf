76eaedbcdd893bc84a17eb7e2326eb76d0b5e0d5
Shreyastc1 <135942784+Shreyastc1@users.noreply.github.com>
2024-05-09 15:07:54 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,13 +2,7 @@ output "storage_account_id" {
   value = azurerm_storage_account.example.id
 }
 
-output "storage_account_primary_access_key" {
-  value = azurerm_storage_account.example.primary_access_key
-}
 
-output "storage_account_secondary_access_key" {
-  value = azurerm_storage_account.example.secondary_access_key
-}
 
 output "storage_account_primary_blob_endpoint" {
   value = azurerm_storage_account.example.primary_blob_endpoint
