71c038f110eed52c5d8565992c78a9c87e337148
Shreyastc1 <135942784+Shreyastc1@users.noreply.github.com>
2024-05-10 13:42:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  # Configuration options
+  features {}
 }
 
 resource "azurerm_resource_group" "example" {
@@ -14,13 +14,9 @@ resource "azurerm_storage_account" "example" {
   account_kind                 = var.account_kind
   account_tier                 = var.account_tier
   access_tier                  = var.access_tier
-  replication_type             = var.replication_type
+  enable_https_traffic_only    = var.enable_https_traffic_only
   allow_blob_public_access     = var.allow_blob_public_access
   allow_nested_items_to_be_public = var.allow_nested_items_to_be_public
-  enable_https_traffic_only    = var.enable_https_traffic_only
-  key_vault_id                 = var.key_vault_id
-  key_vault_key_id             = var.key_vault_key_id
-  key_vault_key_name           = var.key_vault_key_name
 
   tags = var.tags
 }
