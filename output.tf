4687dd81adc441ebe12e094860aebc0c4a524db7
Shreyastc1 <135942784+Shreyastc1@users.noreply.github.com>
2024-05-10 14:03:06 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -10,14 +10,4 @@ output "storage_account_https_only_enabled" {
   value = azurerm_storage_account.example.enable_https_traffic_only
 }
 
-#output "storage_account_key_vault_id" {
-  value = azurerm_storage_account.example.key_vault_id
-}
 
-#output "storage_account_key_vault_key_id" {
-  value = azurerm_storage_account.example.key_vault_key_id
-}
-
-#output "storage_account_key_vault_key_name" {
-  value = azurerm_storage_account.example.key_vault_key_name
-}
