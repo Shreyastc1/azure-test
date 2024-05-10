7d9a80d8c2a938c8f1c08cec2a803ca2c3ace08d
Shreyastc1 <135942784+Shreyastc1@users.noreply.github.com>
2024-05-10 13:40:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.103.0"
-    }
-  }
+provider "azurerm" {
+  # Configuration options
 }
 
 resource "azurerm_resource_group" "example" {
